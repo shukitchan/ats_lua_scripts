1073be537a27ac7fbd522bfb088f74318db96e16
Kit Chan <kichan@apache.org>
2018-05-05 19:42:30 -0700

    Initial version

---
+++ connect_geoip.lua
@@ -0,0 +1,15 @@
+-- depends on "luajit-geoip"
+
+-- Setup Instructions
+-- 1) install GeoIP - 1.6.12
+-- 2) install GeoIP legacy country database - https://dev.maxmind.com/geoip/legacy/install/country/
+-- 3) install luajit-geoip (https://github.com/leafo/luajit-geoip) or just copy geoip/init.lua from the repo to /usr/local/share/lua/5.1/geoip/init.lua  
+
+ts.add_package_path('/usr/local/share/lua/5.1/?.lua')
+
+local geoip = require 'geoip'
+
+function do_global_send_response()
+  local res = geoip.lookup_addr("8.8.8.8")
+  ts.client_response.header['X-Country'] = res.country_code
+end
