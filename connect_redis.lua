12fa1ab1a87a7a3367eb520f4c16459bd5076ff7
Kit Chan <kichan@apache.org>
2020-03-28 16:44:37 -0700

    Update prerequisite instructions

--- connect_redis.lua
+++ connect_redis.lua
@@ -1,5 +1,33 @@
--- depends on "redis-lua" 2.0.4 - https://github.com/nrk/redis-lua
--- redis-lua depends on LuaSocket v3.0-rc1 - https://github.com/diegonehab/luasocket
+-- Prerequisite 0 - Use ATS 8.0.x
+
+-- Prerequisite 1 - Luajit-2.1.0 beta3/Lua 5.1.4/LuaSocket 3.0 rc1/redis-lua 2.0.4
+
+-- # Install luajit 2.1.0 beta 3
+-- curl -R -O http://luajit.org/download/LuaJIT-2.1.0-beta3.tar.gz && \
+-- tar zxf LuaJIT-2.1.0-beta3.tar.gz && \
+-- cd LuaJIT-2.1.0-beta3 && \
+-- make && \
+-- sudo make install
+-- # Install Lua 5.1.4
+-- curl -R -O http://www.lua.org/ftp/lua-5.1.4.tar.gz && \
+-- tar zxf lua-5.1.4.tar.gz && \
+-- cd lua-5.1.4 && \
+-- make linux test && \
+-- sudo make linux install 
+-- # Install LuaSocket v3.0-rc1 
+-- wget https://github.com/diegonehab/luasocket/archive/v3.0-rc1.tar.gz && \
+-- tar zvf v3.0-rc1.tar.gz && \
+-- cd luasocket-3.0-rc1 && \
+-- sed -i "s/LDFLAGS_linux=-O -shared -fpic -o/LDFLAGS_linux=-O -shared -fpic -L\/usr\/lib -lluajit-5.1 -o/" src/makefile && \
+-- ln -sf /usr/lib/libluajit-5.1.so.2.1.0 /usr/lib/libluajit-5.1.so && \
+-- make && \
+-- sudo make install-unix
+-- # Install redis-lua 2.0.4
+-- wget https://github.com/nrk/redis-lua/archive/v2.0.4.tar.gz && \
+-- tar zxf v2.0.4.tar.gz && \
+-- sudo cp redis-lua-2.0.4/src/redis.lua /usr/local/share/lua/5.1/redis.lua
+
+-- Prerequisite 2 - Set up redis-server with unix domain socket
 
 -- unix domain socket has better performance and so we should set up local redis to use that
 -- Note the sock must be readable/writable by nobody since ATS runs as that user
