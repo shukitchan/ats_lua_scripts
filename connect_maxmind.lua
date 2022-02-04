f3dcdd69db574afe138eab947bbfa10f1eef97b8
Kit Chan <kichan@apache.org>
2022-02-03 23:55:14 -0800

    Update connect_maxmind.lua to use luajit-geoip

--- connect_maxmind.lua
+++ connect_maxmind.lua
@@ -1,39 +1,24 @@
--- depends on "luajit-geoip"
+-- Use maxmind GeoLite2 database to return information from an IP address
 
 -- Setup Instructions
--- 1) install libmaxminddb - 1.4.2 (https://github.com/maxmind/libmaxminddb)
--- 2) Get GeoLite2 country database (GeoLite2-Country.mmdb) from https://dev.maxmind.com/geoip/geoip2/geolite2/ and put it in /usr/local/var/lua/
--- 3) Get lua-resty-maxminddb (https://github.com/anjia0532/lua-resty-maxminddb.git) 
---   a) git clone https://github.com/anjia0532/lua-resty-maxminddb.git
---   b) cd lua-resty-maxminddb
---   c) git checkout d00c9f32224b4603f2e049b1edf2ddee9dd5b1bc
---   d) cp lib/resty/maxminddb.lua /usr/local/share/lua/5.1/
+-- 1) install libmaxminddb - 1.6.0 (https://github.com/maxmind/libmaxminddb)
+-- 2) Get GeoLite2 country database (GeoLite2-Country.mmdb) from https://dev.maxmind.com/geoip/geolite2-free-geolocation-data and put it in /usr/local/var/lua/
+-- 3) Get luajit-geoip 2.1.0(https://github.com/leafo/luajit-geoip) 
+--   a) wget https://github.com/leafo/luajit-geoip/archive/refs/tags/v2.1.0.tar.gz
+--   b) tar zxvf v2.1.0.tar.gz
+--   c) cd luajit-geoip-2.1.0/geoip
+--   d) mkdir -p /usr/local/share/lua/5.1/geoip
+--   e) cp *.lua /usr/local/share/lua/5.1/geoip
 
 ts.add_package_path('/usr/local/share/lua/5.1/?.lua')
 
-local geo = require 'maxminddb'
-geo.init("/usr/local/var/lua/GeoLite2-Country.mmdb") 
-
-function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      ts.debug(formatting)
-      tprint(v, indent+1)
-    else
-      ts.debug(formatting .. v)
-    end
-  end
-end
+local geoip = require 'geoip.mmdb'
 
 function do_global_send_response()
-  local res,err = geo.lookup("8.8.8.8")
-   
-  if not res then
-    ts.client_response.header['X-Maxmind-Info'] = 'failed to lookup by ip ,reason:' .. err
-  else 
-    tprint(res)
-    ts.client_response.header['X-Maxmind-Info'] = 'check result in traffic.out with debug on'
-  end 
+  local mmdb = geoip.load_database("/usr/share/GeoIP/GeoLite2-Country.mmdb")
+
+  local result = mmdb:lookup("8.8.8.8")
+
+  ts.client_response.header['X-Maxmind-Info'] = result.country.iso_code
 end
+
