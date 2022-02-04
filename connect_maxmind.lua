e2c1bc3e7a299b963d7a22d4d7de9f636654b60f
Kit Chan <kichan@apache.org>
2022-02-04 15:20:23 -0800

    Update connect_maxmind.lua

--- connect_maxmind.lua
+++ connect_maxmind.lua
@@ -2,7 +2,7 @@
 
 -- Setup Instructions
 -- 1) install libmaxminddb - 1.6.0 (https://github.com/maxmind/libmaxminddb)
--- 2) Get GeoLite2 country database (GeoLite2-Country.mmdb) from https://dev.maxmind.com/geoip/geolite2-free-geolocation-data and put it in /usr/local/var/lua/
+-- 2) Get GeoLite2 country database (GeoLite2-Country.mmdb) from https://dev.maxmind.com/geoip/geolite2-free-geolocation-data and put it in /usr/share/GeoIP/GeoLite2-Country.mmdb
 -- 3) Get luajit-geoip 2.1.0(https://github.com/leafo/luajit-geoip) 
 --   a) wget https://github.com/leafo/luajit-geoip/archive/refs/tags/v2.1.0.tar.gz
 --   b) tar zxvf v2.1.0.tar.gz
