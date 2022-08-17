418a60a4a025c81aa7604abc320cff34f143e53b
Kit Chan <kichan@apache.org>
2022-08-17 16:43:14 -0700

    Sample script to resize image on the fly

---
+++ imagick.lua
@@ -0,0 +1,42 @@
+-- Resize image on the fly
+
+-- Prerequisite 1 - make sure you have imagick 
+
+-- sudo yum install ImageMagick-devel
+
+-- Prerequisite 2 - Download, Compile and install lua-imagick
+-- Assume that luajit library can be found under /usr/lib64, we need to make the imagick.so library to look for it through dynamic link
+
+-- git clone https://github.com/isage/lua-imagick.git
+-- cd lua-imagick
+-- sed -i "s/target_link_libraries(imagick \${ImageMagick_LIBRARIES})/target_link_libraries(imagick \${ImageMagick_LIBRARIES} -L\/usr\/lib64 -lluajit-5.1)/" CMakeLists.txt
+-- mkdir build
+-- cd build
+-- cmake ..
+-- make
+-- sudo make install
+
+-- Put this file in /usr/local/var/lua/imagick.lua and use this in a line in remap.config
+-- e.g. map https://www.test.com/test.jpg https://origin.test.com/test.jpb @tslua.so @pparam=/usr/local/var/lua/imagick.lua
+
+ts.add_package_cpath("/usr/lib64/lua/5.1/?.so")
+local magick = require "imagick"
+
+function transform(data, eos)
+  ts.ctx["image"] = ts.ctx["image"] .. data
+  
+  if eos == 1 then
+    local img = magick.open_blob(ts.ctx["image"])
+    img:thumbnail(15,15)
+    local blob, len = img:blob()
+    return blob, eos
+  end
+  
+  return '', eos
+end
+
+function do_remap()
+  ts.hook(TS_LUA_RESPONSE_TRANSFORM, transform)
+  ts.ctx["image"] = ""
+  return TS_LUA_REMAP_NO_REMAP
+end
